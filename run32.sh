6b3e0e1b48f13c81719b76474b195a9abdb7fd3c
zicog <zicog@example.com>
2020-01-03 15:44:16 +0200

    Added rusn32 for the T(s) = 200, 32 bit problem

---
+++ run32.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "-------------------------------"
+uname -a
+
+echo "-------------------------------"
+echo "prune32 (C)"
+time ./prune32
+
+echo "-------------------------------"
+echo "pqplum32 (C + pthreads)"
+time ./pqplum32
+
+echo "-------------------------------"
+echo "tatami_rust_threaded64 (Rust + rayon)"
+time ./tatami_rust_threaded32 200
+
+echo "-------------------------------"
