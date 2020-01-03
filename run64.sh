91de12b585e9377f4ece053bf78ee75e9eb3abcf
michael <michael@conveqs.fi>
2020-01-03 06:14:14 +0000

    Tweaked run64.sh

--- run64.sh
+++ run64.sh
@@ -4,15 +4,15 @@ echo "-------------------------------"
 uname -a
 
 echo "-------------------------------"
-echo "prune64"
+echo "prune64.c"
 time ./prune64
 
 echo "-------------------------------"
-echo "pqplum64"
+echo "pqplum64.c"
 time ./pqplum64
 
 echo "-------------------------------"
-echo "tatami_rust_threaded64"
+echo "tatami_rust_threaded64.rs"
 time ./tatami_rust_threaded64 1000
 
 echo "-------------------------------"
